cc542eda14b0e565d252ffb9bb5e0457a1380c03
Hakan Cunier <hakan.cunier@gmail.com>
2022-09-07 14:05:48 +0300

    imp

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -10,107 +10,109 @@ import classnames from 'classnames';
 import Container from '../containers/Container';
 
 const Item = ({
-  id,
-  icon,
-  label,
-  to,
-  externalLink,
-  hasSubMenu,
-  active,
-  hasActiveChild,
-  subMenuVisibility,
-  toggleSubMenu,
-  activateMe,
-  reduxStoreName,
-  reduxUid,
-  badgeClass,
-  badgeValue
+    id,
+    icon,
+    label,
+    to,
+    externalLink,
+    hasSubMenu,
+    active,
+    hasActiveChild,
+    subMenuVisibility,
+    toggleSubMenu,
+    activateMe,
+    reduxStoreName,
+    reduxUid,
+    badgeClass,
+    badgeValue,
+    notAuthorized
 }, {
-  classStore,
-  LinkComponent,
+    classStore,
+    LinkComponent,
 }) => (
-  <li
-    className={classnames(
-      classStore.classItem,
-      active && classStore.classItemActive,
-      hasActiveChild && classStore.classItemHasActiveChild,
-      (hasSubMenu && subMenuVisibility) && classStore.classItemHasVisibleChild,
-    )}
-  >
-    <LinkComponent
-      className={classStore.classLink}
-      classNameActive={classStore.classLinkActive}
-      classNameHasActiveChild={classStore.classLinkHasActiveChild}
-      active={active}
-      hasActiveChild={hasActiveChild}
-      id={id}
-      to={to}
-      label={label}
-      externalLink={externalLink}
-      hasSubMenu={hasSubMenu}
-      toggleSubMenu={toggleSubMenu}
-      activateMe={activateMe}
-    >
-      <i className={classnames(classStore.classIcon, classStore.iconNamePrefix + icon)} />
-      {label}
-      {badgeValue && <span className={"badge "+(badgeClass?badgeClass:"")}>{badgeValue}</span>}
-      {hasSubMenu && <i
+    <li
         className={classnames(
-          classStore.classStateIcon,
-          classStore.iconNamePrefix + (
-            subMenuVisibility
-              ? classStore.iconNameStateVisible
-              : classStore.iconNameStateHidden
-          ),
+            classStore.classItem,
+            active && classStore.classItemActive,
+            hasActiveChild && classStore.classItemHasActiveChild,
+            (hasSubMenu && subMenuVisibility) && classStore.classItemHasVisibleChild,
         )}
-      />}
-    </LinkComponent>
-    {hasSubMenu && <Container
-      itemId={id}
-      visible={subMenuVisibility}
-      reduxStoreName={reduxStoreName}
-      reduxUid={reduxUid}
-    />}
-  </li>
+    >
+        <LinkComponent
+            className={classStore.classLink}
+            classNameActive={classStore.classLinkActive}
+            classNameHasActiveChild={classStore.classLinkHasActiveChild}
+            active={active}
+            hasActiveChild={hasActiveChild}
+            id={id}
+            to={to}
+            label={label}
+            externalLink={externalLink}
+            hasSubMenu={hasSubMenu}
+            toggleSubMenu={toggleSubMenu}
+            activateMe={activateMe}
+        >
+            <i className={classnames(classStore.classIcon, classStore.iconNamePrefix + icon, notAuthorized ? "not-authorized" : null)} />
+            {label}
+            {badgeValue && <span className={"badge " + (badgeClass ? badgeClass : "")}>{badgeValue}</span>}
+            {notAuthorized && <span className={"not-authorized-indicator " + (badgeClass ? badgeClass : "")}></span>}
+            {hasSubMenu && <i
+                className={classnames(
+                    classStore.classStateIcon,
+                    classStore.iconNamePrefix + (
+                        subMenuVisibility
+                            ? classStore.iconNameStateVisible
+                            : classStore.iconNameStateHidden
+                    ),
+                )}
+            />}
+        </LinkComponent>
+        {hasSubMenu && <Container
+            itemId={id}
+            visible={subMenuVisibility}
+            reduxStoreName={reduxStoreName}
+            reduxUid={reduxUid}
+        />}
+    </li>
 );
 
 Item.defaultProps = {
-  icon: '',
-  label: '',
-  to: null,
-  externalLink: false,
-  toggleSubMenu: null,
+    icon: '',
+    label: '',
+    to: null,
+    externalLink: false,
+    toggleSubMenu: null,
 };
 
 Item.propTypes = {
-  id: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string,
-  ]).isRequired,
-  icon: PropTypes.string,
-  label: PropTypes.oneOfType([
-    PropTypes.element,
-    PropTypes.array,
-    PropTypes.string,
-  ]),
-  to: PropTypes.string,
-  externalLink: PropTypes.bool,
-  hasSubMenu: PropTypes.bool.isRequired,
-  active: PropTypes.bool.isRequired,
-  hasActiveChild: PropTypes.bool.isRequired,
-  subMenuVisibility: PropTypes.bool.isRequired,
-  toggleSubMenu: PropTypes.func,
-  activateMe: PropTypes.func.isRequired,
-  reduxStoreName: PropTypes.string.isRequired,
-  reduxUid: PropTypes.number.isRequired,
+    id: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string,
+    ]).isRequired,
+    icon: PropTypes.string,
+    label: PropTypes.oneOfType([
+        PropTypes.element,
+        PropTypes.array,
+        PropTypes.string,
+    ]),
+    to: PropTypes.string,
+    externalLink: PropTypes.bool,
+    hasSubMenu: PropTypes.bool.isRequired,
+    active: PropTypes.bool.isRequired,
+    hasActiveChild: PropTypes.bool.isRequired,
+    subMenuVisibility: PropTypes.bool.isRequired,
+    toggleSubMenu: PropTypes.func,
+    activateMe: PropTypes.func.isRequired,
+    reduxStoreName: PropTypes.string.isRequired,
+    reduxUid: PropTypes.number.isRequired,
 };
 
 Item.contextTypes = {
-  classStore: PropTypes.object.isRequired,
-  LinkComponent: PropTypes.oneOfType([
-    PropTypes.element,
-    PropTypes.func,
-  ]).isRequired,
+    classStore: PropTypes.object.isRequired,
+    LinkComponent: PropTypes.oneOfType([
+        PropTypes.element,
+        PropTypes.func,
+    ]).isRequired,
 };
 
 export default Item;
